dc2c23ae1a1b0a7aa3524eac02a4d67d2b0f268b
maxife36 <maxife36@gmail.com>
2023-11-03 14:33:16 -0300

    Cambios de envio de form - middleware para encoding a json

--- js/mediciones.js
+++ js/mediciones.js
@@ -11,34 +11,4 @@ sectionSelector.addEventListener("change", () => {
 })
 
 
-const submitButton = document.querySelector("#btn-submit");
 
-
-submitButton.addEventListener("click", (event) => {
-event.preventDefault()
-console.log(this);
-/*     const MeasureInputs = document.querySelectorAll("input[type='number']")
-    const CheckBoxInputs = document.querySelectorAll("input[type='checkbox']")
-    const NoteInput = document.querySelector("#inp-observaciones")
-
-    const measureKeys = [...MeasureInputs].map(el => el.name)
-    const measureValues = [...MeasureInputs].map(el => Number(el.value))
-    const checkboxKeys = [...CheckBoxInputs].map(el => el.name)
-    const checkboxValues = [...CheckBoxInputs].map(el => el.checked)
-    const noteKey = NoteInput.name
-    const noteValue = NoteInput.value
-
-
-
-  let measureData = {horasDeLuz:}
-
-    measureKeys.forEach((el,index) =>{
-        if(el != horasDeLuz ){
-            measureData[el] = measureValues[index]
-        }else{
-            measureValues[index] === horasLuzPredetermiando
-        }
-    })
-    checkboxKeys.forEach((el,index) => measureData[el] = checkboxValues[index] )
-    measureData[noteKey] = noteValue  */
-})
